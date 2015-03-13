b2633e214a269d60098ea9885b406c522e0499af
Chris LaRose <chris.larose@mycase.com>
2015-03-13 06:33:20 -0700

    cjl - Fix amt node list resizing

--- include/amt.h
+++ include/amt.h
@@ -26,22 +26,12 @@ void amtnode_init(AMTNode *node, uint16_t count, uint16_t character) {
 
 void amtnodelist_insert(AMTNode **node, uint16_t *len, int index, char c) {
   // resize node list to accommodate one more entry
-  printf("index: %d\n", index);
-  printf("oldlen: %u\n", *len);
   (*len)++;
-  printf("newlen: %u\n", *len);
-
-  printf("*node before: %p\n", *node);
   *node = (AMTNode *) realloc(*node, *len * sizeof(AMTNode));
-  printf("*node after: %p\n", *node);
 
   // move nodes [index..*len) down one slot
-  printf("sizeof(AMTNode): %zu\n", sizeof(AMTNode));
   size_t sz = sizeof(AMTNode) * (*len - index - 1);
-  printf("size: %zu\n", sz);
-  printf("dest: %p\n", &(*node[index]) + sizeof(AMTNode));
-  printf("src: %p\n", &(*node[index]));
-  memmove(&(*node[index]) + sizeof(AMTNode), &(*node[index]), sz);
+  memmove(&(*node[index]) + 1, &(*node[index]), sz);
 
   // update new node with data
   amtnode_init(&(*node[index]), 0, c);
@@ -54,16 +44,13 @@ void amt_init(BitMappedNode *trie) {
 }
 
 void amt_insert(BitMappedNode *root, const void *value, size_t len) {
-  printf("\n\nINSERT\n");
   int i = 0;
 
   BitMappedNode *trie = root->node->sub_trie;
   if (!trie) {
-    printf("trie: %p\n", trie);
     trie = root->node->sub_trie = (BitMappedNode *) malloc(sizeof(BitMappedNode));
     bitmap_init(&trie->map);
     trie->node = NULL;
-    printf("trie: %p\n", trie);
   }
   assert(trie != NULL);
 
@@ -72,7 +59,6 @@ void amt_insert(BitMappedNode *root, const void *value, size_t len) {
   unsigned char *c = (unsigned char *) value;
 
   for (i = 0; i < len; ++i, ++c) {
-    printf("char: %u %c\n", *c, *c);
 
     // set the character in the bitmap
     bitmap_set(&trie->map, *c, true);
@@ -81,7 +67,6 @@ void amt_insert(BitMappedNode *root, const void *value, size_t len) {
 
     // actually add the node
     int index = bitmap_get_offset(&trie->map, *c);
-    printf("index: %d\n", index);
     amtnodelist_insert(&trie->node, &root->node->node_count, index, *c);
   }
 }
