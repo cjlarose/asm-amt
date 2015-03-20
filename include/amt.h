93b3cfab661f04b0feffc7fc2444c5bafa666b14
Chris LaRose <chris.larose@mycase.com>
2015-03-19 19:18:18 -0700

    Convert Array Mapped Trie to use c++ classes. Simplified storage of AMT nodes with use of vector.

--- include/amt.h
+++ include/amt.h
@@ -1,93 +1,70 @@
 #ifndef AMT_H
 #define AMT_H
  
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
+#include <vector>
 #include "bit_map.h"
 
-typedef struct BitMappedNode {
+class AMTNode;
+
+class BitMappedNode {
   Bitmap map;
-  struct AMTNode *node;
-} BitMappedNode;
+  std::vector<AMTNode> nodes;
 
-typedef struct AMTNode {
-  uint16_t node_count;
-  uint16_t character;
-  BitMappedNode *sub_trie;
-} AMTNode;
+  public:
+    BitMappedNode();
+    void insert(const void *value, size_t len);
+    bool contains(const void *value, size_t len);
+};
 
-void amtnode_init(AMTNode *node, uint16_t count, uint16_t character) {
-  node->node_count = count;
-  node->character = character;
-  node->sub_trie = NULL;
+class AMTNode {
+  public:
+    uint16_t character;
+    BitMappedNode *sub_trie;
+    AMTNode(): character('\0'), sub_trie(NULL) {}
+};
+
+BitMappedNode::BitMappedNode() {
+  nodes.push_back(AMTNode());
 }
 
-void amtnodelist_insert(AMTNode **node, uint16_t *len, int index, char c) {
-  // resize node list to accommodate one more entry
-  (*len)++;
-  *node = (AMTNode *) realloc(*node, *len * sizeof(AMTNode));
-
-  // move nodes [index..*len) down one slot
-  size_t sz = sizeof(AMTNode) * (*len - index - 1);
-
-  AMTNode *src = &(*node)[index];
-  AMTNode *dst = &(*node)[index] + 1;
-  memmove(dst, src, sz);
-
-  // update new node with data
-  amtnode_init(&(*node)[index], 0, c);
-}
-
-void amt_init(BitMappedNode *trie) {
-  bitmap_init(&trie->map);
-  trie->node = (AMTNode *) malloc(sizeof(AMTNode));
-  amtnode_init(trie->node, 0, '\0'); // initialize dummy root node
-}
-
-void amt_insert(BitMappedNode *root, const void *value, size_t len) {
+void BitMappedNode::insert(const void *value, size_t len) {
   int i;
-  BitMappedNode **trie = &root->node->sub_trie;
-  uint16_t *node_count = &root->node->node_count;
+  BitMappedNode **trie = &nodes[0].sub_trie;
   unsigned char *c = (unsigned char *) value;
 
   for (i = 0; i < len; ++i, ++c) {
-    if (!*trie) {
-      *trie = (BitMappedNode *) malloc(sizeof(BitMappedNode));
-      bitmap_init(&(*trie)->map);
-      (*trie)->node = NULL;
-    }
+    if (!*trie)
+      *trie = new BitMappedNode();
 
-    int index = bitmap_get_offset(&(*trie)->map, *c);
+    int index = (*trie)->map.get_offset(*c);
 
     // see if the character is already there
-    if (!bitmap_get(&(*trie)->map, *c)) {
+    if (!(*trie)->map.get(*c)) {
       // set the character in the bitmap
-      bitmap_set(&(*trie)->map, *c, true);
-      bitmap_print(&(*trie)->map);
+      (*trie)->map.set(*c, true);
 
       // actually add the node
-      amtnodelist_insert(&(*trie)->node, node_count, index, *c);
+      AMTNode new_node = AMTNode();
+      new_node.character = *c;
+      (*trie)->nodes.emplace((*trie)->nodes.begin()+index, new_node);
     }
 
-    AMTNode *new_node = &(*trie)->node[index];
+    AMTNode *new_node = &(*trie)->nodes[index];
     trie = &new_node->sub_trie;
-    node_count = &new_node->node_count;
   }
 }
 
-bool amt_contains(BitMappedNode *root, const void *value, size_t len) {
+bool BitMappedNode::contains(const void *value, size_t len) {
   int i;
-  BitMappedNode **trie = &root->node->sub_trie;
+  BitMappedNode **trie = &nodes[0].sub_trie;
   unsigned char *c = (unsigned char *) value;
 
   for (i = 0; i < len; ++i, ++c) {
-    if (!*trie || !bitmap_get(&(*trie)->map, *c))
+    if (!*trie || !(*trie)->map.get(*c))
       return false;
 
-    int index = bitmap_get_offset(&(*trie)->map, *c);
-    AMTNode *node = &(*trie)->node[index];
+    int index = (*trie)->map.get_offset(*c);
+    AMTNode *node = &(*trie)->nodes[index];
     trie = &node->sub_trie;
   }
   return true;
