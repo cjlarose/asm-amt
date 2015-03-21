8a04ac041830c6a3c80402367f74bb95cc0f443d
Chris LaRose <cjlarose@gmail.com>
2015-03-21 12:17:01 -0700

    cjl - insert uses next

--- include/amt.hpp
+++ include/amt.hpp
@@ -51,28 +51,29 @@ bool BitMappedNode::contains(const void *value, size_t len) {
 
 void BitMappedNode::insert(const void *value, size_t len) {
   int i;
-  BitMappedNode **trie = &nodes[0].sub_trie;
   unsigned char *c = (unsigned char *) value;
+  AMTNode *node = &nodes[0];
 
   for (i = 0; i < len; ++i, ++c) {
-    if (!*trie)
-      *trie = new BitMappedNode();
+    AMTNode *tmp = node->next(*c);
+    if (!tmp) break;
+    node = tmp;
+  }
 
-    int index = (*trie)->map.get_offset(*c);
+  for (; i < len; ++i, ++c) {
+    if (!node->sub_trie)
+      node->sub_trie = new BitMappedNode();
 
-    // see if the character is already there
-    if (!(*trie)->map.get(*c)) {
-      // set the character in the bitmap
-      (*trie)->map.set(*c, true);
+    node->sub_trie->map.set(*c, true);
 
-      // actually add the node
-      AMTNode new_node = AMTNode();
-      new_node.character = *c;
-      (*trie)->nodes.emplace((*trie)->nodes.begin()+index, new_node);
-    }
+    int index = node->sub_trie->map.get_offset(*c);
+    AMTNode new_node = AMTNode();
+    new_node.character = *c;
 
-    AMTNode *new_node = &(*trie)->nodes[index];
-    trie = &new_node->sub_trie;
+    std::vector<AMTNode> *node_list = &node->sub_trie->nodes;
+    node_list->emplace(node_list->begin() + index, new_node);
+
+    node = &node_list->at(index);
   }
 }
 
