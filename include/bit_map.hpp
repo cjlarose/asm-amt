f4fc48cbb4778dd3dee1d254a432b5a798b28b79
Chris LaRose <cjlarose@gmail.com>
2015-03-20 20:30:15 -0700

    Update bit_count with implementation from Hacker's delight

--- include/bit_map.hpp
+++ include/bit_map.hpp
@@ -23,11 +23,14 @@ uint32_t bit_set(uint32_t bits, int index, bool value) {
   return value ? bits | (1 << index) : bits & ~(1 << index);
 }
 
-int bit_count(uint32_t bits) {
-  int count;
-  for (count = 0; bits; bits >>= 1)
-    count += bits & 1;
-  return count;
+// Hacker's Delight, p 66
+int bit_count(uint32_t x) {
+  x = x - ((x >> 1) & 0x55555555);
+  x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
+  x = (x + (x >> 4)) & 0x0F0F0F0F;
+  x = x + (x >> 8);
+  x = x + (x >> 16);
+  return x & 0x0000003F;
 }
 
 /******************************************************************************
